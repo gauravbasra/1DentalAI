c5c89520a1298d1b0fa508fb18666a0cfa0afc8e
Gaurav Basra <63831351+gauravbasra@users.noreply.github.com>
2026-05-20 21:37:46 -0600

    Allow production server action origin

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["162.243.186.191", "localhost:3001", "127.0.0.1:3001"],
+    },
+  },
 };
 
 export default nextConfig;
